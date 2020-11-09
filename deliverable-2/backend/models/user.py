87928214ec87a406d2df881ec4c738b0e1afd489
JSYura <paupaulasun@gmail.com>
2020-11-09 14:51:47 +0800

    track backend models

---
+++ deliverable-2/backend/models/user.py
@@ -0,0 +1,31 @@
+import mongoengine as db
+from flask_login import UserMixin
+import string
+import random
+
+MONGO_URL = "mongodb+srv://user1:user1@cluster0.gjkej.mongodb.net/test"
+
+
+
+    # "mongodb+srv://dbUser:CancerChat@cluster0.4ppll." \
+    #         "mongodb.net/test?authSource=admin&replicaSet=atlas-n77zyo-shard-0&read" \
+    #         "Preference=primary&appname=MongoDB%20Compass&ssl=true"
+
+db.connect('malecare-dev', host=MONGO_URL, username='dbUser',
+           password='CancerChat', authentication_source='admin',
+           ssl=True, ssl_cert_reqs='CERT_NONE')
+
+
+class User(db.Document, UserMixin):
+    user_id = db.StringField()
+    email = db.StringField()
+    password = db.StringField()
+
+    def get_id(self):
+        return self.user_id
+
+    def get_password(self):
+        return self.password
+
+    def get_email(self):
+        return self.email
