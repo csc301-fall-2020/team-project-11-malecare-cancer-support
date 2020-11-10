2a650d894d64a989a7935bba777141d643ffe611
JSYura <paupaulasun@gmail.com>
2020-11-10 20:47:08 +0800

    reformat code

--- deliverable-2/backend/models/user.py
+++ deliverable-2/backend/models/user.py
@@ -2,6 +2,7 @@ from flask_login import UserMixin
 
 MONGO_URL = "mongodb+srv://user1:user1@cluster0.gjkej.mongodb.net/test"
 from .mongoengine_connect import db
+
 # "mongodb+srv://dbUser:CancerChat@cluster0.4ppll." \
 #         "mongodb.net/test?authSource=admin&replicaSet=atlas-n77zyo-shard-0&read" \
 #         "Preference=primary&appname=MongoDB%20Compass&ssl=true"
@@ -9,6 +10,7 @@ from .mongoengine_connect import db
 db.connect('malecare-dev', host=MONGO_URL, username='dbUser',
            password='CancerChat', authentication_source='admin',
            ssl=True, ssl_cert_reqs='CERT_NONE')
+
 
 class User(db.Document, UserMixin):
     user_id = db.StringField()
