156ce1d6391a47c70c4101b9c2cf5e7b224ce21e
7hestral <thestral2017@gmail.com>
2020-11-10 03:46:49 -0500

    modify it to contain other preload types

--- deliverable-2/backend/models/preload_data.py
+++ deliverable-2/backend/models/preload_data.py
@@ -1,8 +1,24 @@
 from .mongoengine_connect import db
 
 
-class CancerTypes(db.Document):
+class PreLoads(db.Document):
     cancer_types = db.ListField(db.StringField())
+    sexual_orientations = db.ListField(db.StringField())
+    genders = db.ListField(db.StringField())
+    medications = db.ListField(db.StringField())
+    treatment_types = db.ListField(db.StringField())
 
     def get_cancer_types(self):
         return self.cancer_types
+
+    def get_sexual_orientations(self):
+        return self.sexual_orientations
+
+    def get_genders(self):
+        return self.genders
+
+    def get_medications(self):
+        return self.medications
+
+    def get_treatment_types(self):
+        return self.treatment_types
