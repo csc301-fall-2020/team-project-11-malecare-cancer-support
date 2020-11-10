dc6a2b73f89886f410514900d548407dfea2f8c9
7hestral <thestral2017@gmail.com>
2020-11-10 01:39:20 -0500

    delete unused url

--- deliverable-2/backend/models/user.py
+++ deliverable-2/backend/models/user.py
@@ -1,15 +1,9 @@
 import mongoengine as db
 from flask_login import UserMixin
-import string
-import random
 
-MONGO_URL = "mongodb+srv://user1:user1@cluster0.gjkej.mongodb.net/test"
+# MONGO_URL = "mongodb+srv://user1:user1@cluster0.gjkej.mongodb.net/test"
 
-
-
-    # "mongodb+srv://dbUser:CancerChat@cluster0.4ppll." \
-    #         "mongodb.net/test?authSource=admin&replicaSet=atlas-n77zyo-shard-0&read" \
-    #         "Preference=primary&appname=MongoDB%20Compass&ssl=true"
+MONGO_URL = "mongodb+srv://dbUser:CancerChat@cluster0.4ppll.mongodb.net/test"
 
 db.connect('malecare-dev', host=MONGO_URL, username='dbUser',
            password='CancerChat', authentication_source='admin',
