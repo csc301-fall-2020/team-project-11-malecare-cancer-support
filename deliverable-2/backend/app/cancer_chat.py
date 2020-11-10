49cbde86d72b092e551da0359169a83f631235c6
7hestral <thestral2017@gmail.com>
2020-11-10 02:40:57 -0500

    create api for load cancer types to db

--- deliverable-2/backend/app/cancer_chat.py
+++ deliverable-2/backend/app/cancer_chat.py
@@ -1,6 +1,7 @@
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask import Flask, request
 from ..usecases import login_register_helpers
+from ..usecases import cancer_types_helpers
 
 login_manager = LoginManager()
 app = Flask(__name__)
@@ -15,6 +16,14 @@ app.config["SECRET_KEY"] = 'my secret'
 login_manager.init_app(app)
 
 
+# this is only for initializing empty database,
+# data can be found under backend/app/database_backup/
+@app.route('/load_to_db/cancer_types', methods=['POST'])
+def load_cancer_types():
+    return cancer_types_helpers \
+        .load_to_cancer_type_db(request.get_json()["cancer_types"])
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return login_register_helpers.get_user_by_user_id(user_id)
@@ -25,9 +34,11 @@ def load_user(user_id):
 def index():
     return "your email is " + current_user.get_email()
 
+
 @login_manager.unauthorized_handler
 def unauthorized():
     return "user is not logged in"
+
 
 @app.route("/logout")
 @login_required
