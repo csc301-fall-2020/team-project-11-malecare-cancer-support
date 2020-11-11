7efdb9fe30a374c377392e633f73f7feaf061bd4
JSYura <paupaulasun@gmail.com>
2020-11-12 01:32:31 +0800

    handle info for session

---
+++ deliverable-2/backend/usecases/handle_session_info_helpers.py
@@ -0,0 +1,15 @@
+from ..models.session import Session
+
+
+def is_user_id_existed_in_session_info(user_id):
+    return Session.objects(user_id=user_id).first() is not None
+
+def save_session_id_to_user_id(user_id, session_id):
+    if not is_user_id_existed_in_session_info(user_id):
+        new_session_info = Session(user_id=user_id, session_id= session_id)
+        new_session_info.save()
+    Session.objects(user_id=user_id).update(set__session_id=session_id)
+    return "Save session id successfully."
+
+def get_session_id_by_user_id(user_id):
+    return Session.objects(user_id=user_id).get_session_id()
