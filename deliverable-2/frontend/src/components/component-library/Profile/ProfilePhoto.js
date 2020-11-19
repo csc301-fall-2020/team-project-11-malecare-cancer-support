bfba87122f764c04e1814d67d303697385d1ece7
Ruikai Fang <ruikai.fang@mail.utoronto.ca>
2020-11-19 20:45:34 +0800

    Change profile color and move update button to bottom

--- deliverable-2/frontend/src/components/component-library/Profile/ProfilePhoto.js
+++ deliverable-2/frontend/src/components/component-library/Profile/ProfilePhoto.js
@@ -2,14 +2,12 @@ import React from "react";
 import { Row, Col, Image } from "antd";
 import Avatar from "../../component-library/Profile/UploadUserPhoto";
 import "antd/dist/antd.css";
+import UserPhoto from "../../../assets/UserPhoto.png";
 
 const ProfilePhoto = () => {
   return (
     <Col span={8}>
-      <Image
-        width={250}
-        src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-      />
+      <Image width={250} src={UserPhoto} />
       <p></p>
       <Avatar></Avatar>
     </Col>
