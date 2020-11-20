20ff0d19530a2355a62f267646187020fe26cbee
RuiZhu <v.zhu@mail.utoronto.ca>
2020-11-20 11:20:13 +0800

    add: try catch admin send msg

--- deliverable-2/backend/app/cancer_chat.py
+++ deliverable-2/backend/app/cancer_chat.py
@@ -1,4 +1,5 @@
 import functools
+import sys
 
 import pymongo
 from flask import Flask, jsonify, request
@@ -269,36 +270,41 @@ def save_session():
 @admin_only
 @authenticated_only
 def admin_send_msg(input_json):
-    gender = input_json['includeGenders']
-    age_range = age_list_parser(input_json['includeAges'])
-    include_cancer = input_json['includeCancerTypes']
-    exclude_cancer = input_json['excludeCancerTypes']
-    include_medication = input_json['includeMedications']
-    exclude_medication = input_json['excludeMedications']
-    include_treatment = input_json['includeTreatments']
-    exclude_treatment = input_json['excludeTreatments']
-    message = input_json["message"]
-    for age_min, age_max in age_range:
-        uid_lst = administrator_filter_helpers.get_user_id_from_admin_filter(
-            include_cancer=include_cancer,
-            include_medication=include_medication,
-            include_treatment=include_treatment,
-            exclude_cancer=exclude_cancer,
-            exclude_medication=exclude_medication,
-            exclude_treatment=exclude_treatment,
-            age_min=age_min,
-            age_max=age_max,
-            gender=gender
-        )
-        print(uid_lst)
+    try:
+        gender = input_json['includeGenders']
+        age_range = age_list_parser(input_json['includeAges'])
+        include_cancer = input_json['includeCancerTypes']
+        exclude_cancer = input_json['excludeCancerTypes']
+        include_medication = input_json['includeMedications']
+        exclude_medication = input_json['excludeMedications']
+        include_treatment = input_json['includeTreatments']
+        exclude_treatment = input_json['excludeTreatments']
+        message = input_json["message"]
+        for age_min, age_max in age_range:
+            uid_lst = administrator_filter_helpers.get_user_id_from_admin_filter(
+                include_cancer=include_cancer,
+                include_medication=include_medication,
+                include_treatment=include_treatment,
+                exclude_cancer=exclude_cancer,
+                exclude_medication=exclude_medication,
+                exclude_treatment=exclude_treatment,
+                age_min=age_min,
+                age_max=age_max,
+                gender=gender
+            )
+            print(uid_lst)
 
-        for uid in uid_lst:
-            sid = handle_session_info_helpers.get_session_id_by_user_id(uid)
-            print(sid)
-            message_handle_helper.create_new_text_msg(sender_uid=current_user.get_id(),
-                                                      receiver_uid=uid, text=message)
-            socketio.emit('chat', "send to all filter users",
-                          room=sid)
+            for uid in uid_lst:
+                sid = handle_session_info_helpers.get_session_id_by_user_id(uid)
+                print(sid)
+                message_handle_helper.create_new_text_msg(sender_uid=current_user.get_id(),
+                                                          receiver_uid=uid, text=message)
+                socketio.emit('chat', "send to all filter users",
+                              room=sid)
+        socketio.emit('to_admin', "Successfully sent", room=request.sid)
+    except:
+        e = sys.exc_info()[0]
+        socketio.emit('to_admin', "<p>Error: %s</p>" % e , room=request.sid)
 
 
 @socketio.on('new_friend_request')
