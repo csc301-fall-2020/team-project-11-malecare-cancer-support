939c502b17fc1230ed46674912a9904024ae306c
JSYura <paupaulasun@gmail.com>
2020-11-21 15:43:00 +0800

    add helper of return email list of query from input info

--- deliverable-2/backend/usecases/administrator_filter_helpers.py
+++ deliverable-2/backend/usecases/administrator_filter_helpers.py
@@ -38,3 +38,20 @@ def get_user_id_from_admin_filter(include_cancer, exclude_cancer, include_treatm
 
     return query_result
 
+def get_email_from_admin_filter(include_cancer, exclude_cancer, include_treatment, exclude_treatment, include_medication, exclude_medication, gender, age_min, age_max):
+    start_year = datetime.datetime(datetime.datetime.utcnow().year - age_max, 1, 1)
+    end_year = datetime.datetime(datetime.datetime.utcnow().year - age_min, 12, 31)
+    query_result = User.objects(Q(cancer__in=include_cancer) &
+                                Q(cancer__nin=exclude_cancer) &
+                                Q(treatments__exists=True) &
+                                Q(treatments__in=include_treatment) &
+                                Q(treatments__nin=exclude_treatment) &
+                                Q(medications__exists=True) &
+                                Q(medications__in=include_medication) &
+                                Q(medications__nin=exclude_medication) &
+                                Q(gender__in=gender) &
+                                Q(date_of_birth__exists=True) &
+                                Q(date_of_birth__gte=start_year) &
+                                Q(date_of_birth__lte=end_year)).only('user_id').values_list('email')
+
+    return query_result
