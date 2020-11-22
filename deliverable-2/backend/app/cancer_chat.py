31e79bbd256f24080e74a827ff7a5d0bdbf24e4e
RuiZhu <v.zhu@mail.utoronto.ca>
2020-11-22 10:56:11 +0800

    fix: undecided friend requests not showing up

--- deliverable-2/backend/app/cancer_chat.py
+++ deliverable-2/backend/app/cancer_chat.py
@@ -375,7 +375,12 @@ def accept_friend_request(payload):
 @app.route('/friend_requests/decline', methods=['POST'])
 @login_required
 def decline_friend_request():
-    _friend_request_helper(request.get_json(),
+    receiver_id = current_user.get_id()
+    sender_id = request.get_json()['sender']
+    print({"receiver": receiver_id,
+                            "sender": sender_id})
+    _friend_request_helper({"receiver": receiver_id,
+                            "sender": sender_id},
                            friend_handler_helpers.decline_friend_request)
     return "declined", 200
 
--- deliverable-2/backend/models/friend_request.py
+++ deliverable-2/backend/models/friend_request.py
@@ -4,4 +4,4 @@ from .mongoengine_connect import db
 class FriendRequest(db.Document):
     sender_uid = db.StringField()
     receiver_uid = db.StringField()
-    is_accepted = db.BooleanField(default=False)
+    is_accepted = db.BooleanField(required=False)
