289586e9162e7dbec96897625d0f15c6f528157a
XiaomHu <huxm1204@gmail.com>
2020-11-28 16:06:18 -0500

    Fixed :id passin in changePassword page

--- deliverable-2/frontend/src/components/pages/customer-page/account-page/ResetPassword.js
+++ deliverable-2/frontend/src/components/pages/customer-page/account-page/ResetPassword.js
@@ -8,12 +8,12 @@ import { message } from "antd";
 import { UserContext } from "../../../../contexts/UserContext";
 import { getCurrentUser } from "../../../utils/helpers";
 import {
-    PageTitleSection,
-    PageSubTitleSection,
-    Space,
-    RegularMessage,
-    WarningMessage,
-    PrimaryButton,
+  PageTitleSection,
+  PageSubTitleSection,
+  Space,
+  RegularMessage,
+  WarningMessage,
+  PrimaryButton,
 } from "../../../share-styled-component";
 import { HOST_URL } from "../../../utils/sharedUrl";
 
@@ -38,79 +38,78 @@ const SectionDescription = styled.div`
   padding: 12px 0;
 `;
 
-const ResetPassword = ({ token }) => {
-    const { user, setUser } = useContext(UserContext);
-    const history = useHistory();
+const ResetPassword = ({ match }) => {
+  const { user, setUser } = useContext(UserContext);
+  const history = useHistory();
+  const token = _.get(match, "params.id");
 
-    useEffect(() => {console.log(token)}, [])
+  useEffect(() => {
+    // console.log(token);
+    console.log(_.get(match, "params.id"));
+  }, []);
 
-    useEffect(() => {
-        const verifyToken = async () => {
-            axios
-                .post(HOST_URL + "/reset_password/verify", { token: token })
-                .then((res) => {
-                    message.success(res.data)
-                    return true
-                })
-                .catch((err) => {
-                    console.log(err)
-                    return false
-                })
+  useEffect(() => {
+    const verifyToken = async () => {
+      axios
+        .post(HOST_URL + "/reset_password/verify", { token: token })
+        .then((res) => {
+          message.success(res.data);
+          return true;
+        })
+        .catch((err) => {
+          console.log(err);
+          return false;
+        });
+    };
+    const fetchUser = async () => {
+      const verifed = await verifyToken();
+      if (verifed) {
+        const fetchedUser = await getCurrentUser();
+        if (!fetchedUser) {
+          // User not logged in
+          history.push("/");
+        } else if (fetchedUser.is_admin) {
+          // User is admin
+          history.push("/adminSendMessages");
+        } else {
+          // User fetched and updated
+          setUser(fetchedUser);
         }
-        const fetchUser = async () => {
-            const verifed = await verifyToken()
-            if (verifed) {
-                const fetchedUser = await getCurrentUser();
-                if (!fetchedUser) {
-                    // User not logged in
-                    history.push("/");
-                } else if (fetchedUser.is_admin) {
-                    // User is admin
-                    history.push("/adminSendMessages");
-                } else {
-                    // User fetched and updated
-                    setUser(fetchedUser);
-                }
-            } else {
-                history.push("/");
-            }
+      } else {
+        history.push("/");
+      }
+    };
 
+    fetchUser();
+  }, [history, setUser, token]);
 
-        };
+  const handleChangePassword = () => {};
 
-        fetchUser();
-    }, [history, setUser, token]);
-
-    const handleChangePassword = () => {
-
-    }
-
-    return (
-        <MainContainer>
-          <PageTitleSection>Account Settings</PageTitleSection>
-          <Space height="36px" />
-          <SectionContainer>
-            <div>
-              <PageSubTitleSection>Change Password</PageSubTitleSection>
-              <SectionDescription>
-                <RegularMessage>
-                  To change the password of your account, click "Change Password"
-                  button.
-                </RegularMessage>
-                <RegularMessage>
-                  We will then send you an email for setting up the new password.
-                </RegularMessage>
-              </SectionDescription>
-            </div>
-            <div>
-              <PrimaryButton onClick={handleChangePassword}>
-                Change password
-              </PrimaryButton>
-            </div>
-          </SectionContainer>
-        </MainContainer>
-      );
-
-}
+  return (
+    <MainContainer>
+      <PageTitleSection>Account Settings</PageTitleSection>
+      <Space height="36px" />
+      <SectionContainer>
+        <div>
+          <PageSubTitleSection>Change Password</PageSubTitleSection>
+          <SectionDescription>
+            <RegularMessage>
+              To change the password of your account, click "Change Password"
+              button.
+            </RegularMessage>
+            <RegularMessage>
+              We will then send you an email for setting up the new password.
+            </RegularMessage>
+          </SectionDescription>
+        </div>
+        <div>
+          <PrimaryButton onClick={handleChangePassword}>
+            Change password
+          </PrimaryButton>
+        </div>
+      </SectionContainer>
+    </MainContainer>
+  );
+};
 
 export default ResetPassword;
