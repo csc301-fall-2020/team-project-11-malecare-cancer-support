13c3e698c0428c88414dbf64d115455dba73411a
7hestral <thestral2017@gmail.com>
2020-12-02 19:47:07 +0800

    add more helpers, refactor

--- deliverable-2/backend/usecases/match_helpers.py
+++ deliverable-2/backend/usecases/match_helpers.py
@@ -17,7 +17,6 @@ def find_match(cancer_type_lst, sex_orientation_lst, gender_lst, purpose_lst,
                                      date_of_birth__lte=end_year).to_json()
     user_lst = json.loads(query_result_json)
     for u in user_lst:
-        date_of_birth = User.objects(user_id=u['user_id']).first().get_json()[
-            "date_of_birth"]
+        date_of_birth = User.objects(user_id=u['user_id']).first().get_json()["date_of_birth"]
         u['date_of_birth'] = date_of_birth
     return user_lst
--- deliverable-2/backend/util/helpers.py
+++ deliverable-2/backend/util/helpers.py
@@ -1,5 +1,7 @@
 # use for parsing list of strings of ages ["0-10", "11-20", ...]
 import datetime
+import sys
+
 
 def age_list_parser(age_lst):
     result = []
@@ -11,6 +13,7 @@ def age_list_parser(age_lst):
             result.append((int(age_min), int(age_max)))
     return result
 
+
 def get_start_end_year(age_min, age_max):
     start_year = datetime.datetime(datetime.datetime.utcnow().year - age_max, 1,
                                    1)
@@ -18,11 +21,24 @@ def get_start_end_year(age_min, age_max):
                                  31)
     return start_year, end_year
 
+
 def email_lst_to_dict(email_lst):
     output = {"email": []}
     for email in email_lst:
         output["email"].append(email)
     return output
+
+
+def _friend_request_helper(user_dict, func):
+    func(user_dict["sender"],
+         user_dict["receiver"])
+
+
+def print_error():
+    e = sys.exc_info()
+    for i in e:
+        print(i)
+
 
 if __name__ == '__main__':
     print(age_list_parser(["0-10"]))
