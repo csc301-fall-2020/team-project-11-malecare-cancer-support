670ed515dbaed0649c873d16f99b4ed0d180eb86
7hestral <thestral2017@gmail.com>
2020-12-02 21:01:36 +0800

    add try catch for login page

--- deliverable-2/backend/app/login_page.py
+++ deliverable-2/backend/app/login_page.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import current_user, login_user
+import pymongo
 
 from ..usecases import handle_report_helpers, login_register_helpers
 
@@ -10,16 +11,23 @@ login_page = Blueprint('login_page', __name__,
 
 @login_page.route('/login', methods=['POST'])
 def login():
-    user_email = request.get_json()["email"]
-    if not login_register_helpers.email_already_existed(user_email):
-        return "Email or password is not correct", 412
-    if handle_report_helpers.check_user_in_black_list_by_email(user_email):
-        return "This account has been locked", 412
-    if login_register_helpers.verify_password_by_email(email=user_email,
-                                                       password=
-                                                       request.get_json()[
-                                                           "password"]):
-        login_user(login_register_helpers.get_user_by_email(email=user_email))
-        return jsonify(current_user.get_json())
-    else:
-        return "Email or password is not correct", 412
+    try:
+        user_email = request.get_json()["email"]
+        if not login_register_helpers.email_already_existed(user_email):
+            return "Email or password is not correct", 412
+        if handle_report_helpers.check_user_in_black_list_by_email(user_email):
+            return "This account has been locked", 412
+        if login_register_helpers.verify_password_by_email(email=user_email,
+                                                           password=
+                                                           request.get_json()[
+                                                               "password"]):
+            login_user(login_register_helpers.get_user_by_email(email=user_email))
+            return jsonify(current_user.get_json())
+        else:
+            return "Email or password is not correct", 412
+    except pymongo.errors as e:
+        print(e)
+        return "Network connection failed", 500
+    except Exception as e:
+        print(e)
+        return "Error occurs", 500
