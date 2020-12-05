273ba17578f3170d8cd2263093fa0ad8b05bafcc
7hestral <thestral2017@gmail.com>
2020-12-05 15:26:49 +0800

    add: scripts for deployment

--- deliverable-2/backend/app/config.py
+++ deliverable-2/backend/app/config.py
@@ -6,7 +6,7 @@ class Configuration:
     SECRET_KEY = 'my secret'
     API_KEY = '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'
     ROUTE_URL = "http://ec2-52-36-24-67.us-west-2.compute.amazonaws.com:5000"\
-        if os.environ.get("AWS_URL") == "True"\
+        if os.environ.get("AWS_URL") and os.environ.get("AWS_URL") == "True"\
         else "http://localhost:5000"
     # ROUTE_URL = "http://localhost:5000"
     # ROUTE_URL = "http://ec2-52-36-24-67.us-west-2.compute.amazonaws.com:5000"
