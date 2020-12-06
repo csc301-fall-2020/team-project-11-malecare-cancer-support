f46da3e0e314d912723fb1f23ec04185e042f266
7hestral <thestral2017@gmail.com>
2020-12-06 14:39:48 +0800

    modify scripts for deployment

--- deliverable-2/prod_build_local_v2.sh
+++ deliverable-2/prod_build_local_v2.sh
@@ -4,6 +4,6 @@ npm install
 npm run build:prod
 rm -r node_modules
 cd ..
-zip cancerChat.zip frontend backend
+zip cancerChat.zip .
 chmod 400 CancerChatTestingServer.pem
 scp -i "CancerChatTestingServer.pem" frontend/build.zip ubuntu@ec2-52-36-24-67.us-west-2.compute.amazonaws.com:~/
