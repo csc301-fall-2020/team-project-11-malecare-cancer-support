afe599648c6b94f4a9b85f427c0a638bd279b0f1
7hestral <thestral2017@gmail.com>
2020-12-12 00:49:02 +0800

    change config for deployment

--- deliverable-2/backend/app/config.py
+++ deliverable-2/backend/app/config.py
@@ -2,9 +2,9 @@ import os
 
 
 class Configuration:
-    MAIL = "Thestral2017@gmail.com"
+    MAIL = "info@malecare.org"
     SECRET_KEY = 'my secret'
-    API_KEY = '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'
+    API_KEY = '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'
     ROUTE_URL = "http://ec2-52-36-24-67.us-west-2.compute.amazonaws.com:5000"\
         if os.environ.get("AWS_URL") and os.environ.get("AWS_URL") == "True"\
         else "http://localhost:5000"
--- deliverable-2/backend/models/mongoengine_connect.py
+++ deliverable-2/backend/models/mongoengine_connect.py
@@ -2,8 +2,9 @@ import mongoengine as db
 
 # MONGO_URL = "mongodb+srv://user1:user1@cluster0.gjkej.mongodb.net/test"
 
-MONGO_URL = "mongodb+srv://dbUser:CancerChat@cluster0.4ppll.mongodb.net/test"
+# MONGO_URL = "mongodb+srv://dbUser:CancerChat@cluster0.4ppll.mongodb.net/test"
 
-db.connect('malecare-dev', host=MONGO_URL, username='dbUser',
-           password='CancerChat', authentication_source='admin',
+MONGO_URL = "mongodb+srv://DarrylDarryl:e9Jx3Nya^45@cluster0.02idn.mongodb.net/test"
+db.connect('malecare-dev', host=MONGO_URL, username='DarrylDarryl',
+           password='e9Jx3Nya^45', authentication_source='admin',
            ssl=True, ssl_cert_reqs='CERT_NONE')
